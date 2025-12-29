1f2ece55f0f80361d6bbec4d8aaa6abe1f740a82
Mirabel1556 <abimirabel5@gmail.com>
2025-12-29 02:57:34 -0800

    Add files via upload

--- Groupe_15_Tpe_Cloud_2026_exo4.py
+++ Groupe_15_Tpe_Cloud_2026_exo4.py
@@ -1,11 +1,17 @@
 def est_palindrome(n):
-     s=str(n)
-     reverse= s[:: -1]
-     if s == reverse:
-         return f"{n} est palindrome"
-     else:
-         return f"{n} n'est pas palindrome"
-# demadez a l'utilisateur d'entrer un nombre
-n=input("entrez un nombre :")
-#appelons la fonction pour determiner si le nombre est palindrome
-print(est_palindrome(n))
+  #convertir les nombres en chaine pour permettre l'inversion  
+     n_str=str(n)
+   #comparaison de la chaine avec son inverse
+     return n_str == n_str[:: -1]
+""" la fonction prend en entre une liste de nombres et retourne uniquement une liste de nombres palindrome"""
+def nombres_palindromes(l):
+    #comprehension de liste pour trouver les palindromes
+    return[n for n in l if est_palindrome(n)]
+#demande a l'utilisateur d'entrer une liste de nombres
+s=input("entrez une liste de nombres : ")
+""" convertir les nombres en entiers et les garde dans une liste"""
+N=[int(x) for x in s.split()]
+palindrome= nombres_palindromes(N)
+"""affiches la liste de nombres palindromes trouvés """
+print("les nombres palindromes sont:", palindrome)
+       
